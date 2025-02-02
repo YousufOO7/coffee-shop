300ae4de2afb2fdb40f499c040e50deee070a96c
Yousuf007 <yousufjobaer@gmail.com>
2025-02-02 19:53:26 +0600

    added the footer section

--- src/Components/OfferCoffee/OfferCoffee.jsx
+++ src/Components/OfferCoffee/OfferCoffee.jsx
@@ -1,7 +1,7 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
-import { JackInTheBox } from "react-awesome-reveal";
+import { JackInTheBox, Roll } from "react-awesome-reveal";
 
 
 const OfferCoffee = () => {
@@ -21,7 +21,7 @@ const OfferCoffee = () => {
 
     return (
         <div className="my-20 max-w-6xl mx-auto">
-            <JackInTheBox delay={1000}>
+            <JackInTheBox>
                 <h2 data-aos="fade-up" className="text-3xl md:text-4xl lg:text-5xl text-center my-3 font-bold w-5/6 md:w-3/6 mx-auto">
                     We Offer Coffee at Most Crazy Prices
                 </h2>
@@ -30,7 +30,7 @@ const OfferCoffee = () => {
             <section className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8 px-16 lg:px-0 mt-10">
                 {/* Coffee Card */}
                 {
-                    coffees.map(coffee => <div data-aos="fade-up" key={coffee.id} className="border relative border-brown-700 rounded-lg p-6 shadow-lg text-center h-[480px] max-w-sm hover:bg-[#623620] hover:text-white transition-colors duration-500">
+                    coffees.map(coffee => <Roll delay={1000} key={coffee.id}><div data-aos="fade-up" className="border relative border-brown-700 rounded-lg p-6 shadow-lg text-center h-[480px] max-w-sm hover:bg-[#623620] hover:text-white transition-colors duration-500">
 
                         <h3 className="text-xl lg:text-2xl font-bold text-brown-800">{coffee.name}</h3>
                         <div className="flex justify-center items-center my-2">
@@ -50,7 +50,7 @@ const OfferCoffee = () => {
                         <div className="absolute lg:-mt-16 left-22 lg:left-32">
                             <button className="btn bg-[#623620] hover:scale-105 transition-all duration-500  text-white hover:bg-black hover:text-white shadow-md">Add To Card</button>
                         </div>
-                    </div>)
+                    </div> </Roll>)
                 }
             </section>
         </div>
